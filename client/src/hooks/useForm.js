bbac60071c6ef0245f626616451c7986d3a664c9
Freet8707 <freet8707@gmail.com>
2020-07-17 20:46:23 -0500

    project's useForm hook successfully implemented and console log verifies that data updates dynamically for form's values state

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,22 @@
 // write your custom hook here to control your checkout form
+import { useState, useEffect } from 'react'
+
+export const useForm = (initialValue) => {
+    const [values, setValues] = useState(initialValue)
+    const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+
+    const handleChanges = (e) => {
+        setValues({ ...values, [e.target.name]: e.target.value });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setShowSuccessMessage(true);
+    };
+
+    useEffect(() => {
+        console.log(values.firstName)
+    }, [values])
+    
+    return [values, handleChanges, handleSubmit, showSuccessMessage]
+}
